b4a855f224c87c6c00f512a373f25d22a651dd6b
Art Rodriguez <art.rodriguez@live.com>
2017-10-04 08:31:51 -0400

    add report view to app

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,13 @@ class DashboardController < ApplicationController
   def index
     if !!current_user
       @user = current_user
-      @tickets = @user.tickets
+      @tickets = @user.tickets.where("status = ?", "Open")
     end
+  end
+
+  def show
+    @closed_tickets = current_user.tickets.where("status = ?", "Closed")
+    @overdue_tickets = current_user.tickets.where("due_date < ? AND status = ?", Date.today, "Open")
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   root to: "dashboard#index"
+  get '/reports/closed_overdue_tickets', to: 'dashboard#show'
 
   resources :tickets do
     resources :issues
