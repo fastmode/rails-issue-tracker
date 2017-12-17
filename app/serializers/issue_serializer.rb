fd982c5a47bcf3cd78988809ed386b71dea88c5b
Art Rodriguez <art.rodriguez@live.com>
2017-12-16 21:56:58 -0500

    add belongs_to ticket relationship to issue serializer

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,3 +1,4 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :description, :status, :due_date, :assigned_to, :ticket_id, :created_at, :updated_at
+  belongs_to :ticket
 end
