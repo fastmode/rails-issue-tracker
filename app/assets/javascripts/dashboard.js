879ae84ce4b839671e692a51871de785d6282efd
Art Rodriguez <art.rodriguez@live.com>
2018-01-04 15:26:04 -0500

    refactor Handlebars prototype, add comments

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,33 +1,31 @@
 $(document).on('turbolinks:load', function() {
 
-  // Uses JSON to render all open tickets
+  // Clears out form to remove any remnants
   $("#open-tickets").text("")
+
+  // Uses JSON to render all open tickets with Handlebars
   $(function () {
     $.getJSON('/tickets', function(data) {
-      //console.log(data);
       data.forEach(function(el){
-        console.log(el);
 
         Ticket.prototype.renderLI = function() {
           return Ticket.template(this);
         }
         
+        // Finds template in html and compiles it to Handlebars object
         $(function(){
           Ticket.templateSource = $("#ticket-template").html();
           Ticket.template = Handlebars.compile(Ticket.templateSource);
         });
         
+        // Creates new Ticket object
         var ticket = new Ticket(el)
+
+        // Takes new Ticket object and renders with with Handlebars
         var ticketLi = ticket.renderLI();
-        // ticket.templateSource = $("#ticket-template").html();
-        // ticket.template = Handlebars.compile(ticket.templateSource);
+
+        // Adds newly rendered LI to HTML
         $("#open-tickets").append(ticketLi);
-        
-        // debugger;
-        // var ticketLi = document.createElement('li');
-        // ticketLi.setAttribute('class', 'list-group-item')
-        // ticketLi.innerHTML = `<span class="badge">${el["issues"].length}</span><a href="/tickets/${el["id"]}">${el["title"]}</a>`;
-        // $("#open-tickets").append(ticketLi);
       });
     });
   });
@@ -38,9 +36,6 @@ $(document).on('turbolinks:load', function() {
     this.status = attributes.status;
     this.issue_count = attributes.issues.length;
   }
-
-  
-  
   
   // New Ticket Functionality - Removes Add Ticket button then loads New Ticket form.
   $(function () {
@@ -51,52 +46,5 @@ $(document).on('turbolinks:load', function() {
       });     
       e.preventDefault();
     });
-  });
-  
+  });  
 });
-
-
-
-
-
-// // Building Prototype
-
-// function Ticket(attributes) {
-//   this.title = attributes.title;
-//   this.status = attributes.status;
-// }
-
-// Ticket.prototype.renderLI = function() {
-//   return Ticket.template(this);
-// }
-
-// $(function(){
-//   Ticket.templateSource = $("#ticket-template").html();
-//   Ticket.template = Handlebars.compile(Ticket.templateSource);
-// });
-
-// Ticket.formSubmit = function(e){
-//   e.preventDefault();
-//   var $form = $(this);
-//   var action = $form.attr("action")
-//   var params = $form.serialize();
-
-//   $.ajax({
-//     url: action,
-//     data: params,
-//     dataType: "json",
-//     method: "POST"
-//   })
-//   .success(Ticket.success)
-//   .error(Ticket.error)
-// }
-
-// Ticket.success = function(json) {
-//   var ticket = new Ticket(json);
-//   var ticketLi = ticket.renderLI();
-//   $("#open-tickets").append(ticketLi);
-// }
-
-// Ticket.error = function(response) {
-//   console.log("Something broke man!", response)
-// }
